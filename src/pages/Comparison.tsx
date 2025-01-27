a67efab2d1cd560822f88fda5f9c54a9ac3c4e88
Ki Wook Kim <wook.kim@rwth-aachen.de>
2025-01-27 05:13:11 +0100

    url fix

--- src/pages/Comparison.tsx
+++ src/pages/Comparison.tsx
@@ -25,7 +25,7 @@ const ComparisonPage = () => {
 
 
   useEffect(() => {
-    axios.get(`https://flask-serverless.onrender.com/comparison${country1}/${country2}`).
+    axios.get(`https://flask-serverless.onrender.com/comparison/${country1}/${country2}`).
       then((response) => {
         setBar1Chart(response.data.bar_1_chart);
         setBar2Chart(response.data.bar_2_chart);
